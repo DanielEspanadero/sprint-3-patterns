9d3d23f8794990af38474eada0dedf61e61b5503
Daniel Españadero <developer@daniel-espanadero.com>
2022-01-17 11:29:36 +0100

    Start of singleton logic

---
+++ sprint-3.1-patterns-1/02-Singleton/app.js
@@ -0,0 +1,7 @@
+// Construeix una aplicació que creï diversos Jugadors. Els jugadors podran ser afegits a un Joc, que mostrarà un marcador amb les puntuacions i el guanyador. L'aplicació ha de poder afegir o treure punts a cada jugador perquè el marcador canviï. La classe Marcador ha d'implementar un patró Singleton com a requisit indispensable.
+
+// TODO Crear diversis jugadors.
+// TODO Poder afegir als jugadors a un joc.
+// TODO Mostrar un marcador amb les puntuacions i el guanyador.
+// TODO Permetre afegir o treure punts a un jugador perquè el marcador canviï.
+// TODO La classe marcador ha de implementar un patró singleton.
