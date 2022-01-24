fa5a5d50862f7ffc1fc18c1a9376047f6db52a3b
Daniel Españadero <you@example.com>
2022-01-24 11:14:59 +0100

    Finished Middleware

--- sprint-3.2-patterns-2/01-Middleware/app.js
+++ sprint-3.2-patterns-2/01-Middleware/app.js
@@ -1,24 +1,46 @@
+// TODO Crea en un fitxer inicial una petita aplicació que sumi, resti i multipliqui rebent els paràmetres en un JSON. [X]
+//TODO Crea en un fitxer extern una classe que emmagatzemi middlewares(funcions). [x]
+//TODO Insereix a la invocació middlewares que facin el quadrat, el cub i la divisió entre 2 dels operands incials en cadascuna de les operacions.
+//TODO Invoca les execucions de la suma, la resta i la multiplicació, de manera que es vagin mostrant per la consola les modificacions que es van fent als valors abans del resultat final.
+// * FUENTE DE TODO EL EJERCICIO: https://stackoverflow.com/questions/39318027/how-to-write-a-middleware-class-in-node-js
+
+'use strict'
+
 const Middleware = require('./middleware');
 const numeros = require('./numeros.json');
-const app = new Middleware(calculadora);
 
 
 const calculadora = (operacio, valors) => {
     let resultat;
-
+    
     switch (operacio) {
         case 'suma': resultat = valors.reduce((a, b) => a + b); break;
         case 'resta': resultat = valors.reduce((a, b) => a - b); break;
         case 'multiplicacio': resultat = valors.reduce((a, b) => a * b); break;
         default: break;
     }
-
+    
     return resultat;
 };
 
-app.use((req, next) => {
-    number1 = req[0].a;
-    number2 = req[1].b;
-    console.log(`divisio entre ${number1} i ${number2}: ` + number1 / number2);
+// console.log(calculadora('suma', [numeros[0].a, numeros[0].b]))
+
+const middleware = new Middleware();
+
+middleware.use(function (info, next) {
+    console.log(`El numero ${numeros[1].a} elevat a ${numeros[1].b} es igual a ${numeros[1].a*numeros[1].b}`); 
     next();
 });
+
+middleware.use(function (info, next) {
+    console.log(`El cubo ${numeros[1].a} elevat a ${numeros[1].b} es igual a ${numeros[1].a ** numeros[1].b}`); 
+    next();
+});
+
+middleware.use(function (info, next) {
+    console.log(`La divisió de ${numeros[1].a} entre ${numeros[1].b} es igual a ${numeros[1].a / numeros[1].b}`);
+    next();
+});
+
+// Run the middleware with initial value
+middleware.run(calculadora('suma', [numeros[1].a, numeros[1].b]));
--- sprint-3.2-patterns-2/01-Middleware/middleware.js
+++ sprint-3.2-patterns-2/01-Middleware/middleware.js
@@ -1,32 +1,21 @@
+'use strict'
+
 class Middleware {
-    constructor(target) {
-        this.target = target
-        this.middlewares = []
-        this.req = {}
 
-        const prototype = Object.getPrototypeOf(this.target)
-        const propertyNames = Object.getOwnPropertyNames(prototype)
-        propertyNames.forEach(fn => (fn !== "constructor") ? this.addFn(fn) : null);
+    constructor() {
+        this.middlewares = [];
     }
 
-    use(middleware) {
-        this.middlewares = [...this.middlewares, middleware]
+    use(fn) {
+        this.middlewares.push(fn);
     }
-
-    runMiddlewares(i = 0) {
-        if (i < this.middlewares.length) {
-            this.middlewares[i].call(this, this.req, () => this.runMiddlewares(i + 1))
-        }
+    executeMiddleware(data, done) {
+        this.middlewares.reduceRight((done, next) => () => next(data, done), done)
+            (data);
     }
-
-    addFn(fn) {
-        this[fn] = args => {
-            this.req = args
-            this.req.result = this.target[fn].call(this, this.req)
-            this.runMiddlewares()
-            return this.req.result
-        }
+    run(data) {
+        this.executeMiddleware(data, done => console.log(data));
     }
-}
+};
 
-module.exports = Middleware
+module.exports = Middleware;
