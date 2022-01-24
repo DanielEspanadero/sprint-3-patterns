7eb5a4f4163a8ffd3d7e3d2a3c011e0a355969ea
Daniel Españadero <developer@daniel-espanadero.com>
2022-01-24 17:49:33 +0100

    inici despres derror

---
+++ sprint-3.2-patterns-2/02-Decorator/models/producte.js
@@ -0,0 +1,11 @@
+class Producte {
+    constructor(name, model, price, currency) {
+        this.name = name;
+        this.model = model;
+        this.price = price;
+        this.currency = currency;
+    }
+
+}
+
+module.exports = Producte;
