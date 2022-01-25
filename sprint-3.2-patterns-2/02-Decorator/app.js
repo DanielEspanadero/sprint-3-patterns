52d0a6da4827da95d91ea06650124411f209c87c
Daniel Españadero <developer@daniel-espanadero.com>
2022-01-25 09:56:51 +0100

    creating decorator

--- sprint-3.2-patterns-2/02-Decorator/app.js
+++ sprint-3.2-patterns-2/02-Decorator/app.js
@@ -1,8 +1,12 @@
 //TODO Crea un Decorator en un arxiu que retorni una funció.Aquesta funció efectuarà una conversió de moneda a euros multiplicant pel coeficient de conversió del fitxer adjunt currency_conversions.json en funció de la divisa original.
-//TODO Crea una petita aplicació que calculi el cost d'uns quants Articles en euros a partir de les seves divises incials, aplicant diferents conversions que usin el Decorator del punt anterior.
+//TODO Crea una petita aplicació que calculi el cost d'uns quants Articles en euros a partir de les seves divises incials, aplicant diferents conversions que usin el Decorator del punt anterior. [x]
 // https://stackoverflow.com/questions/48055771/how-do-i-use-and-apply-javascript-decorators
 
 const Producte = require('./models/producte');
+const data = require('./currency_conversions.json')
+
+
+
 
 const iphone = new Producte('iphone', '6s', 200, '€');
 
---
+++ sprint-3.2-patterns-2/02-Decorator/models/decorator.js
@@ -0,0 +1,24 @@
+// TODO Crea un decorator que retorni una funcio.
+// TODO Aquesta funció retornará una conversió de moneda a euros pel coeficient de conversió del fitxer currency_conversions.json
+
+
+const data = require('../currency_conversions.json');
+
+class Decorator {
+    contructor(){
+        this.preu = preu;
+    }
+
+    obtenirPreu(){
+        return this.preu;
+    };
+
+    canviDivisa(){
+        
+    }
+
+
+
+};
+
+module.exports = Decorator;
