9f2c6f05793959b0fda3204964b66af9cab25659
Daniel Españadero <developer@daniel-espanadero.com>
2022-01-25 19:32:22 +0100

    Decorator finalitzat

--- sprint-3.2-patterns-2/02-Decorator/app.js
+++ sprint-3.2-patterns-2/02-Decorator/app.js
@@ -1,13 +1,30 @@
 //TODO Crea un Decorator en un arxiu que retorni una funció.Aquesta funció efectuarà una conversió de moneda a euros multiplicant pel coeficient de conversió del fitxer adjunt currency_conversions.json en funció de la divisa original.
 //TODO Crea una petita aplicació que calculi el cost d'uns quants Articles en euros a partir de les seves divises incials, aplicant diferents conversions que usin el Decorator del punt anterior. [x]
-// https://stackoverflow.com/questions/48055771/how-do-i-use-and-apply-javascript-decorators
 
 const Producte = require('./models/producte');
-const data = require('./currency_conversions.json')
+const Decorator = require('./models/decorator');
 
 
+//* Introducció de productes a la llista. Els parametres son (nom, model, preu, divisa).
+const iphone = new Producte('iphone', '6s', 200, 'USD');
+const samsung = new Producte('samsung', 's10', 190, 'GBP');
+const huawei = new Producte('huawei', 'p30 pro', 1000, 'CAD');
+
+//! Comprovació d'errors: Si es passan Euros com a divisa inicial, ens posará un missatge de que això no es pot fer.
+const nokia = new Producte('nokia', '3310', 100, 'EUR');
+
+//* Decorator: tenim el métode base que es diu obtenirProducte(). El métode decorator es convertidor(divisa);
+const iPhone6s = new Decorator(iphone);
+const samsungS10 = new Decorator(samsung);
+const huaweiP30Pro = new Decorator(huawei);
+const nokia3310 = new Decorator(nokia);
+
+//* El métode convertidor() en fa poder fer la conversió desde qualsevol de les divises del .json sense la necessitat de crear herències.
+iPhone6s.convertidor('EUR');
+samsungS10.convertidor('EUR');
+huaweiP30Pro.convertidor('EUR');
+
+//! Comprobació d'errors: No es pot passar altre divisa que no siguin Euros, si això passa, sortirà un missatge.
+nokia3310.convertidor('USD');
 
 
-const iphone = new Producte('iphone', '6s', 200, '€');
-
-console.log(iphone)
--- sprint-3.2-patterns-2/02-Decorator/models/decorator.js
+++ sprint-3.2-patterns-2/02-Decorator/models/decorator.js
@@ -1,24 +1,33 @@
-// TODO Crea un decorator que retorni una funcio.
-// TODO Aquesta funció retornará una conversió de moneda a euros pel coeficient de conversió del fitxer currency_conversions.json
-
-
 const data = require('../currency_conversions.json');
 
-class Decorator {
-    contructor(){
-        this.preu = preu;
-    }
 
-    obtenirPreu(){
-        return this.preu;
+class Decorator {
+    constructor(producte) {
+        this.producte = producte;
     };
 
-    canviDivisa(){
-        
-    }
+    obtenirProducte() {
+        return this.producte;
+    };
 
+    convertidor(divisa) {
+        try {
+            const regex = new RegExp(this.obtenirProducte().divisa + '_' + divisa);
+            const conversio = this.obtenirProducte().preu * Object.entries(data).find(key => regex.test(key))[1]
 
+            console.log(`\n--- Es farà la conversió de ${this.obtenirProducte().divisa} a ${divisa} ---`);
 
-};
+            console.log(`- ${this.obtenirProducte().nom} ${this.obtenirProducte().model} amb un preu de ${this.obtenirProducte().preu} ${this.obtenirProducte().divisa} costarà ${conversio.toFixed(2)} ${divisa} al canvi de divisa.\n`);
+        } catch (error) {
+            if(divisa !== 'EUR'){
+                console.log('\n--- Per ara la aplicació només fa la conversió a Euros. --- \n');
+            }else if(this.obtenirProducte().divisa === 'EUR'){
+                console.log('\n La aplicació només està preparada per fer la conversió a Euros, no es poden posar Euros com a divisa inicial. \n')
+            }else{
+                console.log(`La divisa actual no existeix.`);
+            }
+        }
+    };
+}
 
 module.exports = Decorator;
